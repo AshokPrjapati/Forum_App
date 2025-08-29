870926276c583e0a0d684a695d73526fd09b04fc
Ashok Kumar <ap271449@gmail.com>
2025-08-29 11:35:51 +0530

    Refactor Toast component for improved readability and consistency

--- Client/src/Custom-Hooks/Toast.ts
+++ Client/src/Custom-Hooks/Toast.ts
@@ -1,34 +1,31 @@
-import React from 'react'
-import { useToast } from '@chakra-ui/react'
-
+import React from "react";
+import { useToast } from "@chakra-ui/react";
 
 export const ToastType = {
-     info: "info",
-     warning: "warning",
-     success: "success",
-     error: "error",
-     loading: "loading"
-}
+  info: "info",
+  warning: "warning",
+  success: "success",
+  error: "error",
+  loading: "loading",
+};
 
 interface IStatus {
-     status: "info" | "warning" | "success" | "error" | "loading" | undefined
+  status: "info" | "warning" | "success" | "error" | "loading" | undefined;
 }
 
 const UseToastMsg = () => {
-     const toast = useToast();
-     const Toast = (msg: string, status: any) => {
-          toast(
-               {
-                    title: `${msg}`,
-                    position: 'top-right',
-                    variant: 'left-accent',
-                    isClosable: true,
-                    duration: 5000,
-                    status,
-               }
-          )
-     }
-     return { Toast, Type: ToastType }
-}
+  const toast = useToast();
+  const Toast = (msg: string, status: any) => {
+    toast({
+      title: `${msg}`,
+      position: "top-right",
+      variant: "left-accent",
+      isClosable: true,
+      duration: 5000,
+      status,
+    });
+  };
+  return { Toast, Type: ToastType };
+};
 
-export default UseToastMsg
+export default UseToastMsg;
