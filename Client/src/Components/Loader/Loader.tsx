cdb948b89fd8533f1f2b420de122f4c319a2dff7
Ashok Kumar <ap271449@gmail.com>
2025-08-29 11:51:48 +0530

    Added Loader component

--- Client/src/Components/Loader/Loader.tsx
+++ Client/src/Components/Loader/Loader.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import './Loader.css'
+import "./Loader.css";
 
-type Props = {}
+type Props = {};
 
-function Loader({ }: Props) {
-     return (
-          <div className="loader-wrapper">
-               <div className="loader-overlay"></div>
-               <div className='spinner'>
-                    Loading
-                    <div className="spinner-sector spinner-sector-red"></div>
-                    <div className="spinner-sector spinner-sector-blue"></div>
-                    <div className="spinner-sector spinner-sector-green"></div>
-               </div>
-          </div>
-     )
+function Loader({}: Props) {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader-overlay"></div>
+      <div className="spinner">
+        Loading
+        <div className="spinner-sector spinner-sector-red"></div>
+        <div className="spinner-sector spinner-sector-blue"></div>
+        <div className="spinner-sector spinner-sector-green"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
