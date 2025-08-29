3de2eafb3f021f7b16f631722f74c55a5a60bf9e
Ashok Kumar <ap271449@gmail.com>
2025-08-29 11:42:47 +0530

    Refactor App component for improved code consistency and readability

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,43 +1,41 @@
-import React, { useEffect, Suspense } from 'react';
+import { useEffect, Suspense } from "react";
 import Application from "./Application";
-import "./App.css"
-import { useDispatch, useSelector } from 'react-redux';
-import { Dispatch } from 'redux';
-import { getFollowing, getUser } from './Redux/Auth/auth.actions';
-import UseToastMsg, { ToastType } from './Custom-Hooks/Toast';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { getAllPost, postLikes } from './Redux/Post/post.actions';
-import Loader from './Components/Loader/Loader';
-import { RootState } from './Redux/store';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "redux";
+import { getFollowing, getUser } from "./Redux/Auth/auth.actions";
+import UseToastMsg, { ToastType } from "./Custom-Hooks/Toast";
+import { Outlet, useNavigate } from "react-router-dom";
+import { postLikes } from "./Redux/Post/post.actions";
+import Loader from "./Components/Loader/Loader";
+import { RootState } from "./Redux/store";
 
 function App() {
-     const { Toast } = UseToastMsg();
-     const navigate = useNavigate();
-     const dispatch: Dispatch<any> = useDispatch();
-     const { loading } = useSelector((store: RootState) => store.auth);
-     const { loading: postLoader } = useSelector((store: RootState) => store.post);
+  const { Toast } = UseToastMsg();
+  const dispatch: Dispatch<any> = useDispatch();
+  const { loading } = useSelector((store: RootState) => store.auth);
+  const { loading: postLoader } = useSelector((store: RootState) => store.post);
 
-     var user = sessionStorage.getItem("user");
-     let data = user ? JSON.parse(user) : null;
+  var user = sessionStorage.getItem("user");
+  let data = user ? JSON.parse(user) : null;
 
-     useEffect(() => {
-          if (!data?.id) return;
-          dispatch(getUser(data.id, Toast));
-          dispatch(getFollowing(data.id, Toast));
-          dispatch(postLikes(data.id));
-          Toast("Welcome in forum", ToastType.success);
-     }, [])
+  useEffect(() => {
+    if (!data?.id) return;
+    dispatch(getUser(data.id, Toast));
+    dispatch(getFollowing(data.id, Toast));
+    dispatch(postLikes(data.id));
+    Toast("Welcome in forum", ToastType.success);
+  }, []);
 
-
-     return (
-          <>
-               <Suspense fallback={<Loader />}>
-                    <Application />
-                    {loading || postLoader && <Loader />}
-                    <Outlet />
-               </Suspense>
-          </>
-     );
+  return (
+    <>
+      <Suspense fallback={<Loader />}>
+        <Application />
+        {loading || (postLoader && <Loader />)}
+        <Outlet />
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
