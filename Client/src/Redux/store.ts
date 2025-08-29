5b59b9734e685081c3b427625fee0c0bdeb4e9e4
Ashok Kumar <ap271449@gmail.com>
2025-08-29 11:54:41 +0530

    added types and rearranged redux store

--- Client/src/Redux/store.ts
+++ Client/src/Redux/store.ts
@@ -1,30 +1,34 @@
-import { legacy_createStore, applyMiddleware, compose, combineReducers } from "redux";
+import {
+  legacy_createStore,
+  applyMiddleware,
+  compose,
+  combineReducers,
+} from "redux";
 import thunk from "redux-thunk";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { Reducer as AuthReducer } from "./Auth/auth.reducer";
 import { Reducer as PostReducer } from "./Post/post.reducer";
 
 declare global {
-     interface Window {
-          __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
-     }
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
 }
-
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
 const RootReducers = {
-     auth: AuthReducer,
-     post: PostReducer
-}
+  auth: AuthReducer,
+  post: PostReducer,
+};
 
-export const store = legacy_createStore(combineReducers(RootReducers), composeEnhancers(applyMiddleware(thunk)));
+export const store = legacy_createStore(
+  combineReducers(RootReducers),
+  composeEnhancers(applyMiddleware(thunk))
+);
 
-
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
 
 type DisppatchFn = () => AppDispatch;
 export const useAppDispatch: DisppatchFn = useDispatch;
