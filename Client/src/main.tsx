352c433214e31c2d4718fa7f994f1b72233f3cf3
Ashok Kumar <ap271449@gmail.com>
2025-10-01 11:24:36 +0530

    Update axios base URL and refactor user controller for improved readability

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -7,7 +7,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // axios.defaults.baseURL = "https://doubtful-puce-macaw.cyclic.app/"
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "http://localhost:8080/";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider>
