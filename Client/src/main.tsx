ee4eba9d5abd00731cb675cb5b364f22e145b2d3
Ashok Kumar <ap271449@gmail.com>
2025-10-07 11:59:28 +0530

    Update axios base URL to point to the production server

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -6,8 +6,8 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// axios.defaults.baseURL = "https://doubtful-puce-macaw.cyclic.app/"
-axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL = "https://forum-backend-e41b.onrender.com/";
+// axios.defaults.baseURL = "http://localhost:8080/";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider>
